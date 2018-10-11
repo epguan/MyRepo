6b906cd6b0b2e70b83d2fb206c74443febe67816
Dill.Gao <epguan@163.com>
2018-10-11 15:11:34 +0800

    Update ConcurrencyPatterns.go

--- ConcurrencyPatterns.go
+++ ConcurrencyPatterns.go
@@ -16,9 +16,8 @@ var wg sync.WaitGroup
 func main() {
 	checkStatus := func(done <-chan interface{}, urls ...string) <-chan Result {
 		results := make(chan Result)
-		go func() {
+		go func() {           // 此处协程的作用是，迅速返回 result，防止程序阻塞，便于后续处理结果
 			defer close(results)
-
 			for _, url := range urls {
 				wg.Add(1)
 				go func(url string) {
