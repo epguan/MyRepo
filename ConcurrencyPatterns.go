ac43639920a0cea51147c156def65ded0784c76a
Dill.Gao <epguan@163.com>
2018-10-11 15:06:24 +0800

    golang 并发模型
    golang 并发模型：
    1、通过 channel 接收结果，协程向父协程返回 result channel，并负责最后将 result channel 关闭
    2、父协程轮询 result channel
    3、协程错误处理，error 与 result 组装成 struct，交给父协程处理 error

---
+++ ConcurrencyPatterns.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+type Result struct {
+	Error    error
+	Response *http.Response
+}
+
+var wg sync.WaitGroup
+
+func main() {
+	checkStatus := func(done <-chan interface{}, urls ...string) <-chan Result {
+		results := make(chan Result)
+		go func() {
+			defer close(results)
+
+			for _, url := range urls {
+				wg.Add(1)
+				go func(url string) {
+					defer wg.Done()
+					var result Result
+					resp, err := http.Get(url)
+					result = Result{Error: err, Response: resp}
+					select {
+					case <-done:
+						return
+					case results <- result:
+					}
+				}(url)
+			}
+			wg.Wait()
+		}()
+		return results
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	urls := []string{"https://www.baidu.com", "https://badhost"}
+	for result := range checkStatus(done, urls...) {
+		if result.Error != nil {
+			fmt.Printf("error: %v \n", result.Error)
+			continue
+		}
+		fmt.Printf("Response: %v\n", result.Response.Status)
+	}
+}
