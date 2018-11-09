c7e00ff47bfa81ced679ad59ad23f88b19fe1d3d
Dill.Gao <epguan@163.com>
2018-11-09 16:08:45 +0800

    Create DeleteOneElemInSlice.go

---
+++ DeleteOneElemInSlice.go
@@ -0,0 +1,9 @@
+a := []string{"A", "B", "C", "D", "E"}
+i := 2
+
+// Remove the element at index i from a.
+a[i] = a[len(a)-1] // Copy last element to index i.
+a[len(a)-1] = ""   // Erase last element (write zero value).
+a = a[:len(a)-1]   // Truncate slice.
+
+fmt.Println(a) // [A B E D]
